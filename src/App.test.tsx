71063eab2f2dd94c6b916f4f1afb99b67e6a3337
Scentjuls <chidynwanya11@gmail.com>
2023-09-05 17:10:27 +0100

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Test if the status is correct when the game is starting
+//   Test if X and O can win with the row one completed
+// Test cell values after click on the reset button
+// Test tie game, one posibility
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [squares, setSquares] = useState<any>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  const handleClick = (value: number) => {
+    if (squares[value] || calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    xIsNext ? (nextSquares[value] = "X") : (nextSquares[value] = "O");
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+type SquareProps = {
+  onSquareClick: () => void;
+  value: any;
+};
+
+const Square = (props: SquareProps) => {
+  const { onSquareClick, value } = props;
+
+  return (
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
+  );
+};
+
+function calculateWinner(squares: any) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log("square", squares[a]);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
